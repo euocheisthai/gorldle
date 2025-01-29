f102ab7ab051fdf7ef640ff4dc9ca4613e2ee064
euochaisthai <skrzeny7@gmail.com>
2025-01-29 21:22:26 +0300

    deps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
+use axum::{
+    routing::{get, post},
+    http::StatusCode,
+    Json, Router,
+};
+
+
 fn main() {
     println!("Hello, world!");
 }
