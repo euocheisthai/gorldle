96d81e064b09b517771a4d2bbd2db1548f8aa609
euochaisthai <skrzeny7@gmail.com>
2025-02-01 16:41:32 +0300

    ssssssssssssssserde

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,66 @@
+use std::env;
+use std::fs;
 
 use serde::{Deserialize, Serialize};
-use serde_json::Result;
-use axum::{
-    routing::{get, post},
-    http::StatusCode,
-    Json, Router,
-};
+use serde_json;
+use serde_json::{Result, Value};
 
+#[derive(Debug,Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")] 
+enum DotaAttribute {
+    Strength,
+    Agility,
+    Intelligence,
+    Universal,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")] 
+enum DotaPosition {
+    Carry,
+    Midlane,
+    Offlane,
+    SoftSupport,
+    HardSupport
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")] 
+enum DotaAttackType {
+    Melee,
+    Ranged,
+    Both
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct DotaEntry {
+    name: Value, // String,
+    attribute: DotaAttribute,
+    position: Vec<DotaPosition>,
+    attack_type: DotaAttackType,
+    release_year: Value, // u8
+}
+
+fn load_profile(profile_path: &str) -> Value {
+    let profile: String = fs::read_to_string(profile_path)
+        .expect("Did you move the config somewhere?");
+
+    let profile_json: Value = serde_json::from_str(&profile)
+        .expect("That's no JSON");
+
+    return profile_json
+}
 
 fn main() {
-    println!("Hello, world!");
+    let current_profile: Value = load_profile("profile_1.json");
+    let profile_id: &Value = &current_profile["profile_id"];
+
+    if let Value::Array(items) = &current_profile["items"] {
+        for item in items {
+            match serde_json::from_value::<DotaEntry>(item.clone()) {
+                Ok(dota_entry) => println!("{:?}", dota_entry),
+                Err(e) => eprintln!("Failed to parse entry: {}", e),
+            }
+        }
+    }
 }
